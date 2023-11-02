a5be3ac7cdc8bd50a21a02e77b79d9ca656dd1c4
paul michael <mopaul@student.lautech.edu.ng>
2023-11-02 16:40:45 +0100

    I just updated the bank module,  sorry about that

--- bank.py
+++ bank.py
@@ -1,25 +1,33 @@
-from flask import Flask, render_template, request
-from bank import AccHolder, display_account_info
+class AccHolder():
+    def __init__(self, account_number, name, bank_name):
+        self.account_number = account_number
+        self.name = name
+        self.bank_name = bank_name
+        self.current_balance = 0
+
+    def deposit(self, amount):
+        self.current_balance += amount
+        return self.current_balance
+
+    def withdrawal(self, amount):
+        if self.current_balance - amount < 0:
+            return f"You can't withdraw more than your current balance ({self.current_balance})"
+        self.current_balance -= amount
+        return self.current_balance
 
 
-app = Flask(__name__)
 
+# I Created a dictionary to store account owners, this will be our database in real time
+account_owners = {
+    "1234567890": {"name": "Alice", "bank_name": "Bank A"},
+    "9876543210": {"name": "Bob", "bank_name": "Bank B"},
+}
 
-
-
-@app.route("/", methods=["GET","POST"])
-def deposite():
-    if request.method == "GET":
-        return render_template("deposite.html")
-    elif request.method == "POST":
-        acc_number = request.form.get("acc_no")
-        acc_name = request.form.get("acc_name")
-
-        acc_holder = display_account_info(acc_number)
-
-        if acc_holder:
-            amount = float(request.form.get("amount"))
-            balance = acc_holder.deposit(amount)
-            return render_template("deposite.html", balance=balance)
-            # return render_template("deposite.html", balance=0)
-
+# This is a function to display account informations
+def display_account_info(account_number):
+    owner_info = account_owners.get(account_number)
+    if owner_info:
+        account_holder = AccHolder(account_number, owner_info["name"], owner_info["bank_name"])
+        return account_holder
+    else:
+        return None
