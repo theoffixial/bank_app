d1f160d116f60b9e92537dfe1e2306239eb9d3fb
theoffixial <145484656+theoffixial@users.noreply.github.com>
2023-11-02 08:01:00 +0000

    this is my corrrected version

--- app.py
+++ app.py
@@ -1,65 +1,76 @@
-from flask import Flask, render_template, redirect, url_for, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_sqlalchemy import SQLAlchemy
+ # step one banking app
+from flask import Flask, render_template, request, session, url_for, redirect
+from function import Bank
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-login_manager = LoginManager(app)
+users = {}
+print(users)
+app.secret_key = 'qwertyuiop1234[]qwddqwwEsdcsd'
 
 
+@app.route('/')
+def homepage():
+    return render_template('homepage.html') 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+@app.route('/register', methods = ['GET','POST'])
+def register():
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    if request.method == 'POST':
+        username = request.form['username']
 
-class SignupForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    submit = SubmitField('Sign up')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        # Create a new user in the database
-        user = User(name=form.name.data, email=form.email.data, password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        # Log the user in
-        login_user(user)
-        flash('Account created successfully!', 'success')
-
-        return redirect(url_for('index'))
-    return render_template('signup.html', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Get the user from the database
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user)
-            flash('Login successful!', 'success')
-            return redirect(url_for('index'))
+       
+        if username in users:
+            print('User alredy exist')
+            return render_template('register.html')
         else:
-            flash('Invalid email or password', 'danger')
+            password = request.form['password']
+            users[username] = password
+            print('succsesful')
+            print(users)
+            return render_template('homepage.html')
+    else:
+        return render_template('register.html')
+@app.route('/deposite', methods=['GET', 'POST'])
+def deposite():
+    if request.method == 'POST':
+        amount = request.form['deposite']
+        user.deposite(amount)
+        print(user.show_details())  # Call the show_details() method
+        return render_template('deposite.html')
+    else:
+        return render_template ('deposite.html')
 
-            return render_template('login.html', form=form)
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/withdrawal')
+def withdrawal():
+    if request.method == 'POST':
+        amount = request.form['withdrawal']
+        user.withdrawal(amount)
+        print(user.show_details())  # Call the show_details() method
+        return render_template('withdrawal.html')
+    else:
+        return render_template ('withdrawal.html')
+ 
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    global user
+    if request.method =='POST':
+        username =request.form['username']
+        password =request.form['password']
+
+        if username in users and users[username] == password:
+            session['user'] = username
+            user = Bank(session['user'])
+            print(user.show_details())
+            print('Login succsesfull')
+            return render_template('deposite.html')
+    else:
+        print('Username dosent exist or incorrect password')
+        return render_template('login.html')
+
+    
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port= '8000',
+    debug=True)
---
+++ function.py
@@ -0,0 +1,41 @@
+#perent class
+class User():
+    def __init__(self, name):
+        self.name = name
+
+    def show_details(self):
+        x = 'Personal details'
+        y = 'name:' + self.name
+        z = 'account balance is $' +str(self.balance)
+        result = [x,y,z]
+        return result     
+
+# child class
+
+class Bank(User):
+    def __init__(self, name):
+        super().__init__(name)
+        self.balance = 0
+
+    def deposite(self,amount):
+        self.amount = int(amount)
+        self.balance = self.balance +self.amount
+        return 'account balance is now $' +str(self.balance)
+    
+    def withdrawal(self, amount):
+        self.amount = int(amount)
+        if self.amount > self.balance:
+            return "Insufficient funds $" +str(self.balance)
+        else:
+            self.balance = self.balance-self.amount
+            return 'account balanceis now $' +str(self.amount)
+    def view_balance(self):
+        return self.show_details()
+    
+mark = Bank('mark')
+print(mark.show_details())
+print(mark.deposite(1000))
+print(mark.show_details())
+print(mark.withdrawal(100))
+print(mark.show_details())
+            
